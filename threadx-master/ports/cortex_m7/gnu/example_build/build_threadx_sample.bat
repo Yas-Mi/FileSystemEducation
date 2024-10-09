dc2210357e333777ef778828981eb7f5674a74b6
Yasu-Mi <hcuymitoh@gmail.com>
2024-10-09 16:41:25 +0900

    commit for now

--- threadx-master/ports/cortex_m7/gnu/example_build/build_threadx_sample.bat
+++
@@ -1,7 +0,0 @@
-set BIN_PATH="C:\Program Files (x86)\Atollic\TrueSTUDIO for STM32 9.3.0\ARMTools\bin"
-set GCC=%BIN_PATH%\arm-atollic-eabi-gcc.exe
-
-%GCC% -c -g -mcpu=cortex-m7 -mfloat-abi=hard -mfpu=fpv5-d16 -mthumb cortexm7_vectors.S
-%GCC% -c -g -mcpu=cortex-m7 -mfloat-abi=hard -mfpu=fpv5-d16 -mthumb cortexm7_crt0.S
-%GCC% -c -g -mcpu=cortex-m7 -mfloat-abi=hard -mfpu=fpv5-d16 -mthumb tx_initialize_low_level.S
-%GCC% -g -mcpu=cortex-m7 -mfloat-abi=hard -mfpu=fpv5-d16 -mthumb -T sample_threadx.ld -ereset_handler -nostartfiles -o sample_threadx.out -Wl,-Map=sample_threadx.map cortexm7_vectors.o cortexm7_crt0.o tx_initialize_low_level.o tx.a
